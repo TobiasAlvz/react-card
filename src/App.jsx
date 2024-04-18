64aff681e7edd6b279cc93c2f54e64bf7f20c184
Tobias Alves <tobiasjose12@gmail.com>
2024-04-18 09:17:38 -0300

    up

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "./contacts";
+
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">Personagens</h1>
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].img}
+        info={contacts[0].info}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].img}
+        info={contacts[1].info}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].img}
+        info={contacts[2].info}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/contacts.js
@@ -0,0 +1,19 @@
+let contacts = [
+  {
+    name: "Satoro Gojo",
+    img: "https://dimensaosete.com.br/static/7fc311549694666167a49cdb0fb1293c/2493a/gojo.webp",
+    info: "Voce é fraco",
+  },
+  {
+    name: "Sukuna",
+    img: "https://tm.ibxk.com.br/2023/11/14/14093517191519.jpg?ims=1200x675",
+    info: "Você é forte",
+  },
+  {
+    name: "Levi",
+    img: "https://gamefera.com/wp-content/uploads/Capitao-Levi-Ackerman-1.webp",
+    info: "Hum",
+  },
+];
+
+export default contacts;
