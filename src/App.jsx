cf4265bda2525a0875b4f529625692b14502cfb0
Tobias Alves <tobiasjose12@gmail.com>
2024-04-20 10:10:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,18 @@ import React from "react";
 import Card from "./Card";
 import contacts from "./contacts";
 import Avatar from "./Avatar";
-
 import "./style.css";
+
+function creatCard(contact) {
+  return <Card name={contact.name} img={contact.img} info={contact.info} />;
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">Personagens</h1>
       <Avatar img="https://i1.sndcdn.com/artworks-C5gl8UDRFK7ydaFh-MkhgaA-t500x500.jpg" />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].img}
-        info={contacts[0].info}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].img}
-        info={contacts[1].info}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].img}
-        info={contacts[2].info}
-      />
+      {contacts.map(creatCard)}
     </div>
   );
 }
