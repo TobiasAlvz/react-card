e3c390602b2b8de2ef57b285539a2f5593e2fa3f
Tobias Alves <tobiasjose12@gmail.com>
2024-04-22 08:49:52 -0300

    Update contacts.js

--- src/contacts.js
+++ src/contacts.js
@@ -1,15 +1,18 @@
 let contacts = [
   {
+    id: 1,
     name: "Satoro Gojo",
     img: "https://dimensaosete.com.br/static/7fc311549694666167a49cdb0fb1293c/2493a/gojo.webp",
     info: "Voce é fraco",
   },
   {
+    id: 2,
     name: "Sukuna",
     img: "https://tm.ibxk.com.br/2023/11/14/14093517191519.jpg?ims=1200x675",
     info: "Você é forte",
   },
   {
+    id: 3,
     name: "Levi",
     img: "https://gamefera.com/wp-content/uploads/Capitao-Levi-Ackerman-1.webp",
     info: "Hum",
